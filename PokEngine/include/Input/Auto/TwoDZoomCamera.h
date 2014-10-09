c4f3debea85bb9dc397f4e74f5c74bad9622a149
Andrew Huertas <ahuertas@student.neumont.edu>
2014-10-09 19:32:25 +0000

    zooming camera
    git-svn-id: https://svn.neumont.edu:8443/svn/andrew_huertas_game_engine/branches/WorkInProgress@629 661beb30-111e-9b45-af84-fb100ded3add

---
+++ PokEngine/include/Input/Auto/TwoDZoomCamera.h
@@ -0,0 +1,27 @@
+#pragma once 
+#include <PokEngineExportHeader.h>
+#include <Core\Component.h>
+class POKENGINE_SHARED TwoDZoomCamera : public Component
+{
+	GameObject* parent;
+	GameObject** tracking;
+	unsigned int numTracking;
+protected:
+	virtual void attatch( GameObject* parent );
+	virtual void detatch();
+public:
+	float minDistance;
+	float maxDistance;
+	float zoomScale;
+	void initialize(unsigned int numObjectsToTrack);
+	void destroy();
+	bool initialized();
+	bool addGameObjectToTrack( GameObject* object );
+	TwoDZoomCamera();
+	virtual void earlyUpdate();
+	virtual void update();
+	virtual void lateUpdate();
+	virtual void earlyDraw();
+	virtual void draw();
+	virtual void lateDraw();
+};
---
+++ PokEngine/src/Input/Auto/TwoDZoomCamera.cpp
@@ -0,0 +1,91 @@
+#include <Input\Auto\TwoDZoomCamera.h>
+#include <Graphics\Camera.h>
+#include <Core\GameObject.h>
+TwoDZoomCamera::TwoDZoomCamera() : parent(0) , minDistance(1) , maxDistance(10) , zoomScale(1), tracking(0) {}
+
+void TwoDZoomCamera::attatch( GameObject* parent )
+{
+	this->parent = parent;
+}
+
+void TwoDZoomCamera::detatch()
+{
+	parent = 0;
+}
+
+void TwoDZoomCamera::initialize( unsigned int numObjectsToTrack )
+{
+	if ( tracking ) destroy();
+	numTracking = numObjectsToTrack;
+	tracking = new GameObject*[numTracking];
+	for ( unsigned int i = 0; i < numTracking; ++i )
+	{
+		tracking[i] = 0;
+	}
+}
+void TwoDZoomCamera::destroy()
+{
+	if ( !tracking ) return;
+	delete[] tracking;
+	tracking = 0;
+	numTracking = 0;
+}
+bool TwoDZoomCamera::initialized()
+{
+	return tracking != 0;
+}
+
+bool TwoDZoomCamera::addGameObjectToTrack( GameObject* object )
+{
+	for ( unsigned int i = 0; i < numTracking; ++i )
+	{
+		if ( !tracking[i] )
+		{
+			tracking[i] = object;
+			return true;
+		}
+	}
+	return false;
+}
+
+void TwoDZoomCamera::earlyUpdate() {}
+void TwoDZoomCamera::update(){}
+void TwoDZoomCamera::lateUpdate()
+{
+	if ( !parent || !tracking ) return;
+	Camera* theCamera = parent->getComponent<Camera>();
+	if ( !theCamera ) return;
+	glm::vec3 averagePosition;
+	unsigned int numPass = 0 ;
+	for ( unsigned int i = 0; i < numTracking; ++i )
+	{
+		if ( tracking[i] )
+		{
+			averagePosition += tracking[i]->translate;
+			++numPass;
+		}
+	}
+	averagePosition /= numPass;
+	float highestDistance = minDistance;
+	for ( unsigned int i = 0; i < numTracking; ++i )
+	{
+		if ( tracking[i] )
+		{
+			float theLength = glm::length( tracking[i]->translate - averagePosition );
+			if ( highestDistance < theLength )
+			{
+				highestDistance = theLength;
+			}
+		}
+		if ( highestDistance > maxDistance )
+		{
+			highestDistance = maxDistance;
+			break;
+		}
+	}
+
+	parent->translate = averagePosition + ( -theCamera->direction * ( highestDistance * zoomScale ) );
+}
+void TwoDZoomCamera::earlyDraw() {}
+void TwoDZoomCamera::draw() {}
+void TwoDZoomCamera::lateDraw() {}
