bfb5f228dfada3d585b587b45b8a3d6280283986
Andrew Huertas <ahuertas@student.neumont.edu>
2014-10-30 04:03:31 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/andrew_huertas_game_engine/branches/WorkInProgress@704 661beb30-111e-9b45-af84-fb100ded3add

--- PokEngine Debug Tools/include/PokEngineDebugExportHeader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef	POKENGINEDEBUGTOOLS_EXPORTS
-#define POKENGINEDEBUGTOOLS_SHARED __declspec( dllexport )
-#else
-#define POKENGINEDEBUGTOOLS_SHARED __declspec( dllimport )
-#endif
