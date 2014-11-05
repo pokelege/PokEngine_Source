f84ecc31e5cfc5509122d099aabce57c0df4573d
Andrew Huertas <ahuertas@student.neumont.edu>
2014-11-05 19:40:30 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/andrew_huertas_game_engine/branches/WorkInProgress@741 661beb30-111e-9b45-af84-fb100ded3add

--- BinaryAssetBuilder/src/main.cpp
+++ BinaryAssetBuilder/src/main.cpp
@@ -1,21 +1,45 @@
 #include <string>
-
+#include <QtCore\QDir>
+#include <QtCore\QDateTime>
 void main( int argc , char** argv )
 {
 	if ( argc < 2 ) return;
 
-	std::string theOutput( argv[1] );
-	std::string theExtension;
-	while ( theOutput.length() > 0 && theOutput.back() != '.' )
+	QDir inputDir( argv[1] );
+	QDir outputDir( argv[2] );
+	QFileInfoList files = inputDir.entryInfoList();
+	for ( unsigned int i = 0; i < files.size(); ++i )
 	{
-		theExtension = theOutput.back() + theExtension;
-		theOutput.pop_back();
+		if ( files.at( i ).isDir() )
+		{
+			processDir( files.at( i ).absoluteDir() , outputDir, inputDir );
+		}
 	}
-	if ( theOutput.length() > 0 && theOutput.back() == '.' )
+}
+
+void processDir(QDir& input, QDir& output, QDir& mainInput)
+{
+	QFileInfoList files = input.entryInfoList();
+	for ( unsigned int i = 0; i < files.size(); ++i )
 	{
-		theExtension = theOutput.back() + theExtension;
-		theOutput.pop_back();
+		if ( files.at( i ).isDir() )
+		{
+			processDir( files.at( i ).absoluteDir() , output, mainInput );
+		}
+	}
+}
+
+void processFile( QFileInfo& file , QDir& output , QDir& mainInput )
+{
+	QDir outputCopy = output;
+	if ( !outputCopy.cd( mainInput.relativeFilePath( file.absoluteDir().absolutePath() ) ) )
+	{
+		output.mkdir( mainInput.relativeFilePath( file.absoluteDir().absolutePath() ) );
+		outputCopy = output;
+		outputCopy.cd( mainInput.relativeFilePath( file.absoluteDir().absolutePath() ) );
 	}
 
+	if ( )
 
+	file.lastModified().toTime_t();
 }
