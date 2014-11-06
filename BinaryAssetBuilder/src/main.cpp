5869883da1b6e4feaa56e9dad4b03bcbca829ec3
Andrew Huertas <ahuertas@student.neumont.edu>
2014-11-06 21:59:14 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/andrew_huertas_game_engine/branches/WorkInProgress@743 661beb30-111e-9b45-af84-fb100ded3add

--- BinaryAssetBuilder/src/main.cpp
+++ BinaryAssetBuilder/src/main.cpp
@@ -1,36 +1,57 @@
 #include <string>
 #include <QtCore\QDir>
 #include <QtCore\QDateTime>
+#include <QtCore\QDebug>
+#include <iostream>
+void processDir( QDir input , QDir output , QDir mainInput );
+void processFile( QFileInfo file , QDir output , QDir mainInput );
 void main( int argc , char** argv )
 {
+	for ( int i = 0; i < argc; ++i )
+	{
+		std::cout << i << " " << argv[i] << std::endl;
+	}
 	if ( argc < 2 ) return;
 
 	QDir inputDir( argv[1] );
 	QDir outputDir( argv[2] );
 	QFileInfoList files = inputDir.entryInfoList();
-	for ( unsigned int i = 0; i < files.size(); ++i )
+	//std::cout << "numfiles " << files.size() << std::endl;
+	for ( int i = 0; i < files.size(); ++i )
 	{
 		if ( files.at( i ).isDir() )
 		{
-			processDir( files.at( i ).absoluteDir() , outputDir, inputDir );
+			qDebug( files.at( i ).fileName().toUtf8() );
+			processDir( files.at( i ).fileName() , outputDir , inputDir );
+		}
+		else
+		{
+			processFile( files.at( i ) , outputDir , inputDir );
 		}
 	}
 }
 
-void processDir(QDir& input, QDir& output, QDir& mainInput)
+void processDir(QDir input, QDir output, QDir mainInput)
 {
 	QFileInfoList files = input.entryInfoList();
-	for ( unsigned int i = 0; i < files.size(); ++i )
+	//std::cout << "numfiles " << files.size() << std::endl;
+	for ( int i = 0; i < files.size(); ++i )
 	{
 		if ( files.at( i ).isDir() )
 		{
-			processDir( files.at( i ).absoluteDir() , output, mainInput );
+			qDebug( files.at( i ).fileName().toUtf8() );
+			processDir( files.at( i ).fileName() , output , mainInput );
+		}
+		else
+		{
+			processFile( files.at( i ) , output , mainInput );
 		}
 	}
 }
 
-void processFile( QFileInfo& file , QDir& output , QDir& mainInput )
+void processFile( QFileInfo file , QDir output , QDir mainInput )
 {
+	std::cout << "copyed" << std::endl;
 	QDir outputCopy = output;
 	if ( !outputCopy.cd( mainInput.relativeFilePath( file.absoluteDir().absolutePath() ) ) )
 	{
@@ -38,8 +59,14 @@ void processFile( QFileInfo& file , QDir& output , QDir& mainInput )
 		outputCopy = output;
 		outputCopy.cd( mainInput.relativeFilePath( file.absoluteDir().absolutePath() ) );
 	}
-
-	if ( )
-
-	file.lastModified().toTime_t();
+	QFileInfo outPutFile = outputCopy.absolutePath() + file.fileName();
+	if ( outPutFile.exists() && outPutFile.lastModified().toTime_t() < file.lastModified().toTime_t() )
+	{
+		QFile::remove( outPutFile.absoluteFilePath() );
+		QFile::copy( file.absoluteFilePath() , outPutFile.absoluteFilePath() );
+	}
+	else
+	{
+		QFile::copy( file.absoluteFilePath() , outPutFile.absoluteFilePath() );
+	}
 }
