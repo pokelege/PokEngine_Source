a0cc08041496973bdae7ac3e893969da52f677b9
Andrew Huertas <ahuertas@student.neumont.edu>
2014-12-08 22:48:43 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/andrew_huertas_game_engine/branches/WorkInProgress@797 661beb30-111e-9b45-af84-fb100ded3add

--- PokEngine/include/Input/XBox360Xinput.h
+++ PokEngine/include/Input/XBox360Xinput.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <PokEngineExportHeader.h>
-
-class POKENGINE_SHARED XBox360Input
+#include <glm.hpp>
+class POKENGINE_SHARED XBox360XInput
 {
 public:
-	static bool isDown( unsigned int key );
-	static bool wasPressed( unsigned int key );
+	static bool isDown( unsigned short key, unsigned int player = 0 );
+	static float leftTrigger( unsigned int player = 0 );
+	static float rightTrigger( unsigned int player = 0 );
+	static glm::vec2 leftStick( unsigned int player = 0 );
+	static glm::vec2 rightStick( unsigned int player = 0 );
+	static void setVibration( unsigned short left , unsigned short right , unsigned int player = 0 );
+	static bool exists( unsigned int player = 0 );
 };
--- PokEngine/src/Input/XBox360XInput.cpp
+++ PokEngine/src/Input/XBox360XInput.cpp
@@ -1,6 +1,74 @@
-#include <Input\XBox360Xinput.h>
-
-bool XBox360XInput::isDown( unsigned int key , unsigned int player )
+#include <Input\XBox360XInput.h>
+#include <Windows.h>
+#include <Xinput.h>
+bool XBox360XInput::isDown( unsigned short key , unsigned int player )
 {
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	if ( result == ERROR_SUCCESS && (state.Gamepad.wButtons & key) == key )
+	{
+		return true;
+	}
+	else return false;
+}
 
+float XBox360XInput::leftTrigger( unsigned int player )
+{
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	if ( result == ERROR_SUCCESS )
+	{
+		return ((float)state.Gamepad.bLeftTrigger) / 255.0f;
+	}
+	else return 0;
+}
+float XBox360XInput::rightTrigger( unsigned int player )
+{
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	if ( result == ERROR_SUCCESS )
+	{
+		return (( float ) state.Gamepad.bRightTrigger) / 255.0f;
+	}
+	else return 0;
+}
+glm::vec2 XBox360XInput::leftStick( unsigned int player )
+{
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	if ( result == ERROR_SUCCESS )
+	{
+		return glm::vec2(state.Gamepad.sThumbLX, state.Gamepad.sThumbLY);
+	}
+	else return glm::vec2();
+}
+glm::vec2 XBox360XInput::rightStick( unsigned int player )
+{
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	if ( result == ERROR_SUCCESS )
+	{
+		return glm::vec2( state.Gamepad.sThumbLX , state.Gamepad.sThumbLY );
+	}
+	else return glm::vec2();
+}
+void XBox360XInput::setVibration( unsigned short left , unsigned short right , unsigned int player )
+{
+	XINPUT_VIBRATION vibration;
+	vibration.wLeftMotorSpeed = left;
+	vibration.wRightMotorSpeed = right;
+	XInputSetState( player , &vibration );
+}
+
+bool XBox360XInput::exists( unsigned int player )
+{
+	XINPUT_STATE state;
+	ZeroMemory( &state , sizeof( XINPUT_STATE ) );
+	DWORD result = XInputGetState( player , &state );
+	return result == ERROR_SUCCESS;
 }
